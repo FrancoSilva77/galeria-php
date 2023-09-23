ef152184ee872ae417c53e15aece021a170dc65e
Franco Silva <franco_tigres18@hotmail.com>
2023-09-22 19:30:13 -0600

    Funcion comprimir add

--- app/funciones.php
+++ app/funciones.php
@@ -19,3 +19,31 @@ function is_auth(): bool
 
   return false;
 }
+
+function compressImage($source, $destination, $quality)
+{
+  // Obtenemos la información de la imagen
+  $imgInfo = getimagesize($source);
+  $mime = $imgInfo['mime'];
+
+  // Creamos una imagen
+  switch ($mime) {
+    case 'image/jpeg':
+      $image = imagecreatefromjpeg($source);
+      break;
+    case 'image/png':
+      $image = imagecreatefrompng($source);
+      break;
+    case 'image/gif':
+      $image = imagecreatefromgif($source);
+      break;
+    default:
+      $image = imagecreatefromjpeg($source);
+  }
+
+  // Guardamos la imagen
+  imagejpeg($image, $destination, $quality);
+
+  // Devolvemos la imagen comprimida
+  return $destination;
+}
